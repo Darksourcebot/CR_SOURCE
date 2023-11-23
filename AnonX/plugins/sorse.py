3f61b7a81e3a1e77795757b5c31d08caa6dfd393
DEV-E3DAM <139201770+Darksourcebot@users.noreply.github.com>
2023-11-23 14:54:59 +0200

    Add files via upload

---
+++ AnonX/plugins/sorse.py
@@ -0,0 +1,62 @@
+
+import asyncio
+
+import os
+import config
+import time
+import requests
+from config import START_IMG_URL
+from pyrogram import filters
+import random
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from strings.filters import command
+from AnonX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from AnonX import app
+from random import  choice, randint
+@app.on_message(
+    filters.regex(r"(سورس مين|سورس|السورس|سورسي|CR)")
+    & ~filters.edited
+)
+async def huhh(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/e7bb54b34faadd2c9b199.jpg",
+        caption=f"""𝘛𝘏𝘌 𝘉𝘌𝘚𝘛 𝘚𝘖𝘜𝘙𝘊𝘌 𝘖𝘕 𝘛𝘌𝘓𝘌𝘎𝘙𝘈𝘔""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "ꪑꪗ ᦔꫀꪜ", url=f"https://t.me/DAD_E3DAM"), 
+                
+                    InlineKeyboardButton(
+                        "ᧁ𝘳ꪮꪊρ ᥴ𝘳", url=f"https://t.me/POU_V"),
+                ],[
+                    InlineKeyboardButton(
+                        "⌞  𝑪𝑹 • 𝙎𝙊𝙐𝙍𝘾𝙀 ⌝", url=f"https://t.me/SOURCE_CR"),
+                ],
+
+            ]
+
+        ),
+
+    )
+
+
+
+@app.on_message(command(["غنيلي", "غني", "غ", "🎙 ¦ غـنيـلي"]))
+async def ihd(client: Client, message: Message):
+    rl = random.randint(3,267)
+    url = f"https://t.me/bsmaatt/{rl}"
+    await client.send_voice(message.chat.id,url,caption="🔥 ¦ تـم اختيـار الاغـنـية لـك",parse_mode="html",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+    )
+
+
+
